5db0dea8453f83f8fc5ac25357224bd90f85b64b
iTRobert <quangnamhd9x@gmail.com>
2020-09-06 21:27:27 +0700

    update

---
+++ index.php
@@ -0,0 +1,68 @@
+<form method="post">
+    Number: <input id="number" type="number" name="number"><br>
+    <input type="submit" value="Read">
+</form>
+<?php
+$number = $_POST['number'];
+switch ($number) {
+    case 1:
+        echo "one";
+        break;
+    case 2:
+        echo "two";
+        break;
+    case 3:
+        echo "three";
+        break;
+    case 4:
+        echo "four";
+        break;
+    case 5:
+        echo "five";
+        break;
+    case 6:
+        echo "six";
+        break;
+    case 7:
+        echo "seven";
+        break;
+    case 8:
+        echo "eight";
+        break;
+    case 9:
+        echo "nine";
+        break;
+    case 10:
+        echo "ten";
+        break;
+    case 11:
+        echo "eleven";
+        break;
+    case 12:
+        echo "twelve";
+        break;
+    case 13:
+        echo "thirteen";
+        break;
+    case 14:
+        echo "fourteen";
+        break;
+    case 15:
+        echo "fifteen";
+        break;
+    case 16:
+        echo "sixteen";
+        break;
+    case 17:
+        echo "seventeen";
+        break;
+    case 18:
+        echo "eighteen";
+        break;
+    case 19:
+        echo "nineteen";
+        break;
+    case 20:
+        echo "twenty";
+        break;
+}
